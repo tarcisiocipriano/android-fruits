c64288eed4ac4479e4e20cf301b31dd582001f64
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-01-12 23:05:37 -0300

    implement creation of new fruits

--- app/src/main/java/cesar/school/android_fruits/FruitCreationActivity.kt
+++ app/src/main/java/cesar/school/android_fruits/FruitCreationActivity.kt
@@ -1,11 +1,36 @@
 package cesar.school.android_fruits
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import cesar.school.android_fruits.databinding.ActivityFruitCreationBinding
+import cesar.school.android_fruits.model.Fruit
 
 class FruitCreationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFruitCreationBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fruit_creation)
+        binding = ActivityFruitCreationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Toast.makeText(this, "works", Toast.LENGTH_SHORT)
+
+        var newFruit: Fruit
+
+        binding.buttonAddFruitConfirm.setOnClickListener {
+            val name = binding.inputFruitName.text.toString()
+            val benefits = binding.textareaFruitBenefits.text.toString()
+
+            if (!name.isNullOrEmpty() && !benefits.isNullOrEmpty()) {
+                newFruit = Fruit(name, benefits, (0..3).random())
+                val returnIntent = Intent()
+                returnIntent.putExtra(MainActivity.MAIN_ACTIVITY_RESULT_ID, newFruit)
+                setResult(Activity.RESULT_OK, returnIntent)
+                finish()
+            }
+        }
     }
 }
--- app/src/main/java/cesar/school/android_fruits/MainActivity.kt
+++ app/src/main/java/cesar/school/android_fruits/MainActivity.kt
@@ -1,14 +1,22 @@
 package cesar.school.android_fruits
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import cesar.school.android_fruits.adapter.FruitAdapter
 import cesar.school.android_fruits.databinding.ActivityMainBinding
 import cesar.school.android_fruits.model.Fruit
 
+
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val MAIN_ACTIVITY_FRUIT_REQUEST_CODE = 1
+        const val MAIN_ACTIVITY_RESULT_ID = "newFruit"
+    }
 
     private lateinit var binding : ActivityMainBinding
 
@@ -28,9 +36,27 @@ class MainActivity : AppCompatActivity() {
 
         binding.fruitList.adapter = fruitAdapter
         binding.fruitList.layoutManager = GridLayoutManager(this, 1)
+
+        // setup insert button
+        binding.buttonAddFruit.setOnClickListener {
+            val resultActivity = Intent(this, FruitCreationActivity::class.java)
+            startActivityForResult(resultActivity, MAIN_ACTIVITY_FRUIT_REQUEST_CODE)
+        }
     }
 
     private fun onFruitClickListener(fruit: Fruit) {
         Toast.makeText(this, "Fruit: ${fruit.name}", Toast.LENGTH_SHORT).show()
+    }
+
+    // activity result
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (MAIN_ACTIVITY_FRUIT_REQUEST_CODE == requestCode) {
+                val (name, benefits, photo) = data?.getParcelableExtra<Fruit>(MAIN_ACTIVITY_RESULT_ID) as Fruit
+                listFruits.add(Fruit(name, benefits, photo))
+                fruitAdapter.notifyItemInserted(listFruits.lastIndex)
+            }
+        }
     }
 }
