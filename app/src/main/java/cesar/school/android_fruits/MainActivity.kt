94803d2c580a583823af0c234d25ea2cc24424b0
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-01-12 21:52:17 -0300

    implement recycleview

--- app/src/main/java/cesar/school/android_fruits/MainActivity.kt
+++ app/src/main/java/cesar/school/android_fruits/MainActivity.kt
@@ -2,10 +2,35 @@ package cesar.school.android_fruits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import cesar.school.android_fruits.adapter.FruitAdapter
+import cesar.school.android_fruits.databinding.ActivityMainBinding
+import cesar.school.android_fruits.model.Fruit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private val listFruits = mutableListOf(
+        Fruit("Apple", "Lorem ipsum", 0),
+        Fruit("Grape", "Lorem ipsum", 1),
+        Fruit("Orange", "Lorem ipsum", 2),
+        Fruit("Strawberry", "Lorem ipsum", 3)
+    )
+
+    private val fruitAdapter = FruitAdapter(this, listFruits, this::onFruitClickListener)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fruitList.adapter = fruitAdapter
+        binding.fruitList.layoutManager = GridLayoutManager(this, 1)
+    }
+
+    private fun onFruitClickListener(fruit: Fruit) {
+        Toast.makeText(this, "Fruit: ${fruit.name}", Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/cesar/school/android_fruits/adapter/FruitAdapter.kt
+++ app/src/main/java/cesar/school/android_fruits/adapter/FruitAdapter.kt
@@ -1,4 +1,46 @@
 package cesar.school.android_fruits.adapter
 
-class FruitAdapter {
+import android.content.Context
+import android.content.res.TypedArray
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cesar.school.android_fruits.R
+import cesar.school.android_fruits.databinding.ItemFruitBinding
+import cesar.school.android_fruits.model.Fruit
+
+class FruitAdapter(private val context: Context,
+                   private val fruits: List<Fruit>,
+                   private val callback: (Fruit) -> Unit): RecyclerView.Adapter<FruitAdapter.VH>() {
+
+    private val fruitPhotos: TypedArray by lazy {
+        context.resources.obtainTypedArray(R.array.fruitPhotos)
+    }
+
+    class VH(itemView: ItemFruitBinding): RecyclerView.ViewHolder(itemView.root) {
+        val fruitName: TextView = itemView.itemFruitName
+        val fruitBenefits: TextView = itemView.itemFruitDescription
+        val fruitPhoto: ImageView = itemView.itemFruitPhoto
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val binding = ItemFruitBinding.inflate(LayoutInflater.from(parent.context))
+        val vh = VH(binding)
+        vh.itemView.setOnClickListener {
+            val fruit = fruits[vh.adapterPosition]
+            callback(fruit)
+        }
+        return vh
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val (name, benefits, photo) = fruits[position]
+        holder.fruitName.text = name
+        holder.fruitBenefits.text = benefits
+        holder.fruitPhoto.setImageDrawable(fruitPhotos.getDrawable(photo))
+    }
+
+    override fun getItemCount(): Int = fruits.size
 }
