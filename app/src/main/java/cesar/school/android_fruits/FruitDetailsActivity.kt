21d987a039a760e5d6a40c79bcf9b1db5bc213c2
Tarcisio Cipriano <creativecipriano@gmail.com>
2021-01-13 21:36:04 -0300

    implement the imageview on details activity

--- app/src/main/java/cesar/school/android_fruits/FruitDetailsActivity.kt
+++ app/src/main/java/cesar/school/android_fruits/FruitDetailsActivity.kt
@@ -2,6 +2,7 @@ package cesar.school.android_fruits
 
 import android.app.Activity
 import android.content.Intent
+import android.content.res.TypedArray
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -12,18 +13,21 @@ class FruitDetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityFruitDetailsBinding
 
+    private val fruitPhotos: TypedArray by lazy {
+        resources.obtainTypedArray(R.array.fruitPhotos)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFruitDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val fruit = intent.getParcelableExtra<Fruit>(MainActivity.MAIN_ACTIVITY_FRUIT_ID)
+        val (name, benefits, photo) = intent.getParcelableExtra<Fruit>(MainActivity.MAIN_ACTIVITY_FRUIT_ID) as Fruit
         val index = intent.getIntExtra(MainActivity.MAIN_ACTIVITY_FRUIT_INDEX, -1)
 
-        if (fruit != null) {
-            binding.detailsFruitName.text = fruit.name
-            binding.detailsFruitBenefits.text = fruit.benefits
-        }
+        binding.detailsFruitName.text = name
+        binding.detailsFruitBenefits.text = benefits
+        binding.detailsFruitPhoto.setImageDrawable(fruitPhotos.getDrawable(photo))
 
         // remove fruit
         binding.buttonRemoveFruit.setOnClickListener {
